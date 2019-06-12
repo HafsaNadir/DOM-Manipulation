1a7606052bbcc9605cd46b2d75b20334c8021558
HafsaNadir <hafsanadir21@gmail.com>
2019-06-12 19:21:50 +0500

    updating score and toggling active player

--- app.js
+++ app.js
@@ -13,6 +13,25 @@ const handler = () => {
     var diceDOM = document.querySelector('.dice')
     diceDOM.style.display = 'block'
     diceDOM.src = 'dice-' + dice + '.png'
+
+    if(dice !== 1)
+    {
+        roundScore += dice
+        document.querySelector('#current-' + activePlayer).textContent = roundScore
+    }
+    else
+    {
+        document.getElementById('score-' + activePlayer).textContent = roundScore
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+        roundScore = 0
+        document.querySelector('#current-0').textContent = 0
+        document.querySelector('#current-1').textContent = 0
+        
+        document.querySelector('.player-0-panel').classList.toggle('active')
+        document.querySelector('.player-1-panel').classList.toggle('active')
+
+        document.querySelector('.dice').style.display= 'none'
+    }
 }
 
 let scores, roundScore, activePlayer
@@ -21,12 +40,10 @@ roundScore = 0
 activePlayer = 0
 
 document.querySelector('.dice').style.display = 'none'
-
-document.getElementById('score-0').textContent = 0
-document.getElementById('score-1').textContent = 0
 document.getElementById('current-0').textContent = 0
 document.getElementById('current-1').textContent = 0
-
+document.getElementById('score-0').textContent = 0
+document.getElementById('score-1').textContent = 0
 document.querySelector('.btn-roll').addEventListener('click' , handler)
 
 
